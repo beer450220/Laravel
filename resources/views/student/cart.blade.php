30a9d8d8c3fc577a9bef5a406f949ab4122aee9d
beer450220 <107822398+beer450220@users.noreply.github.com>
2023-09-14 11:29:44 +0700

    update

--- resources/views/student/cart.blade.php
+++ resources/views/student/cart.blade.php
@@ -323,9 +323,9 @@
                                            <table id="cart" class="table table-hover table-condensed">
                                             <thead>
                                                 <tr>
-                                                    <th style="width:50%">Product</th>
-                                                    <th style="width:10%">Price</th>
-                                                    <th style="width:8%">Quantity</th>
+                                                    <th style="width:10%">รูปภาพ</th>
+                                                    <th style="width:10%">ชื่อสถานประกอบการ</th>
+                                                    <th style="width:8%">ลบ</th>
 
                                                 </tr>
                                             </thead>
@@ -347,7 +347,7 @@
 
 
                                                             <td class="actions" data-th="">
-                                                                <button class="btn btn-danger btn-sm cart_remove"><i class="fa fa-trash-o"></i> Delete</button>
+                                                                <button class="btn btn-danger btn-sm cart_remove"><i class="fa fa-trash-o"></i>ลบ</button>
                                                             </td>
                                                         </tr>
                                                     @endforeach
